d42f1bab793e6f8119e6b0932d87106a2d796c79
WanderBread <delossantos.wander@gmail.com>
2017-08-27 17:21:13 -0400

    Wireframe from August 27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NavbarComponent,
+  FooterComponent]
 })
 export class AppModule { }
